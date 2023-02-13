02c5b40ad1a9a491772b2238e6f5773ff2a142c9
moppediert <moppediert@gmail.com>
2023-02-13 23:06:39 +0100

    Prepare some stuff for message parsing

---
+++ src/indexing.rs
@@ -0,0 +1,123 @@
+use rosbag::{IndexRecord, RosBag};
+use std::collections::{HashMap, HashSet};
+use std::fs;
+use std::path::Path;
+use std::time::Instant;
+
+pub fn read_bags(path: &Path) -> HashMap<String, RosBag> {
+    let paths = fs::read_dir(path).unwrap();
+    let mut bag_paths = vec![];
+
+    for path in paths {
+        let p = path.unwrap().path();
+        if let Some(bag) = p.extension() {
+            if bag.to_str().unwrap() == "bag" {
+                bag_paths.push(p);
+            }
+        }
+    }
+
+    let mut bags = HashMap::<String, RosBag>::new();
+
+    for bag_path in bag_paths {
+        let bag_path_str = bag_path.file_name().unwrap().to_str().unwrap();
+        let bag = RosBag::new(bag_path_str).unwrap();
+
+        bags.insert(bag_path_str.to_string(), bag);
+    }
+    bags
+}
+
+pub fn read_bag(path: &Path) -> RosBag {
+    let bag_path_str = path.to_str().unwrap();
+    RosBag::new(bag_path_str).unwrap()
+}
+
+pub fn get_topics(bag: &RosBag) -> Vec<String> {
+    let mut topics = vec![];
+    // Iterate over records in the index section
+    for record in bag.index_records() {
+        match record.unwrap() {
+            IndexRecord::Connection(conn) => {
+                topics.push(conn.topic.to_string());
+            }
+            _ => {}
+        }
+    }
+    topics
+}
+
+pub fn get_messages(bag: &RosBag, topic: &str) -> Vec<Vec<u8>> {
+    let mut conn_id = 9999;
+    let mut messages = vec![];
+    let mut sections = "".to_string();
+    let mut num_as_states = 0;
+    let mut conns = HashSet::new();
+    let start = Instant::now();
+    for record in bag.chunk_records() {
+        match record.unwrap() {
+            rosbag::ChunkRecord::Chunk(chunk) => {
+                for message in chunk.messages() {
+                    match message.unwrap() {
+                        rosbag::MessageRecord::MessageData(message_data) => {
+                            if message_data.conn_id == 0 {
+                                messages.push(message_data.data.into());
+                            }
+                        }
+                        rosbag::MessageRecord::Connection(conn) => {
+                            if conn.topic == topic {
+                                conn_id = conn.id;
+                            }
+                        }
+                    }
+                }
+            }
+            rosbag::ChunkRecord::IndexData(index_data) => {
+                conns.insert(index_data.conn_id);
+                if index_data.conn_id == conn_id {
+                    num_as_states += index_data.count;
+                }
+            }
+        }
+    }
+    let mut conns = conns.into_iter().collect::<Vec<u32>>();
+    conns.sort();
+    // println!("{:?}", conns);
+
+    let mut index_section = "".to_string();
+    for record in bag.index_records() {
+        match record.unwrap() {
+            // connection records always come first so conn_id can be read before the first chunk info appears
+            IndexRecord::Connection(conn) => {
+                println!("{}", conn.message_definition);
+                println!("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+                if conn.topic == topic {
+                    conn_id = conn.id;
+                }
+            }
+            IndexRecord::ChunkInfo(chunk_info) => {
+                let num_local_as_states = chunk_info
+                    .entries()
+                    .filter_map(|e| {
+                        if e.conn_id == conn_id {
+                            Some(e.count)
+                        } else {
+                            None
+                        }
+                    })
+                    .sum::<u32>();
+                num_as_states += num_local_as_states;
+            }
+        }
+    }
+    let end = Instant::now();
+    let duration = end - start;
+    // println!("{}", duration.as_secs_f32());
+    // println!("{}", sections);
+    // println!("-----------------------------------------------------------------------------------------------");
+    // println!("{}", index_section);
+    // println!("{:?}", num_as_states);
+    // println!("{:?}", messages.len());
+    // println!("chunk count: {}", bag.get_chunk_count());
+    messages
+}
---
+++ src/message_parsing.rs
@@ -0,0 +1,102 @@
+use byteorder::{ByteOrder, LE};
+use std::str;
+
+fn parse_message_definition() {
+    
+}
+
+fn parse_primivate_data(type_definition: &str, data: &[u8]) -> String {
+    let parsed_data = match type_definition {
+        "bool" => format!("{}", parse_bool(&data[..1]).unwrap()),
+        "int8" => format!("{}", parse_int8(&data[..1]).unwrap()),
+        "uint8" => format!("{}", parse_uint8(&data[..1]).unwrap()),
+        "int16" => format!("{}", parse_int16(&data[..2]).unwrap()),
+        "uint16" => format!("{}", parse_uint16(&data[..2]).unwrap()),
+        "int32" => format!("{}", parse_uint16(&data[..4]).unwrap()),
+        "uint32" => format!("{}", parse_uint32(&data[..4]).unwrap()),
+        "int64" => format!("{}", parse_int64(&data[..8]).unwrap()),
+        "uint64" => format!("{}", parse_uint64(&data[..8]).unwrap()),
+        "float32" => format!("{}", parse_float32(&data[..8]).unwrap()),
+        "float64" => format!("{}", parse_float64(&data[..8]).unwrap()),
+        _ => "".to_string(),
+    };
+    "".to_string()
+}
+
+#[derive(Debug)]
+pub enum Error {
+    MessageParsingError(String),
+}
+
+fn check_len(data_type: &str, data: &[u8], len: usize) -> Result<(), Error> {
+    if data.len() != len {
+        Err(Error::MessageParsingError(format!(
+            "Cannot parse message of type: {}",
+            data_type
+        )))
+    } else {
+        Ok(())
+    }
+}
+
+fn parse_bool(data: &[u8]) -> Result<bool, Error> {
+    check_len("bool", &data, 1).map(|_| data[0] != 0)
+}
+
+fn parse_int8(data: &[u8]) -> Result<i8, Error> {
+    check_len("int8", &data, 1).map(|_| i8::from_le_bytes(data.try_into().unwrap()))
+}
+
+fn parse_uint8(data: &[u8]) -> Result<u8, Error> {
+    check_len("uint8", &data, 1).map(|_| data[0])
+}
+
+fn parse_int16(data: &[u8]) -> Result<i16, Error> {
+    check_len("int16", &data, 2).map(|_| LE::read_i16(data))
+}
+
+fn parse_uint16(data: &[u8]) -> Result<u16, Error> {
+    check_len("uint16", &data, 2).map(|_| LE::read_u16(data))
+}
+
+fn parse_int32(data: &[u8]) -> Result<i32, Error> {
+    check_len("int32", &data, 4).map(|_| LE::read_i32(data))
+}
+
+fn parse_uint32(data: &[u8]) -> Result<u32, Error> {
+    check_len("uint32", &data, 4).map(|_| LE::read_u32(data))
+}
+
+fn parse_int64(data: &[u8]) -> Result<i64, Error> {
+    check_len("int64", &data, 8).map(|_| LE::read_i64(data))
+}
+
+fn parse_uint64(data: &[u8]) -> Result<u64, Error> {
+    check_len("uint64", &data, 8).map(|_| LE::read_u64(data))
+}
+
+fn parse_float32(data: &[u8]) -> Result<f32, Error> {
+    check_len("float32", &data, 4).map(|_| LE::read_f32(data))
+}
+
+fn parse_float64(data: &[u8]) -> Result<f64, Error> {
+    check_len("float64", &data, 8).map(|_| LE::read_f64(data))
+}
+
+fn parse_time(data: &[u8]) -> Result<u32, Error> {
+    check_len("time", &data, 4).map(|_| LE::read_u32(data))
+}
+
+fn parse_duration(data: &[u8]) -> Result<i32, Error> {
+    check_len("duration", &data, 4).map(|_| LE::read_i32(data))
+}
+
+fn parse_str(data: &[u8]) -> Result<String, Error> {
+    match str::from_utf8(data) {
+        Ok(parsed) => Ok(parsed.to_string()),
+        Err(e) => Err(Error::MessageParsingError(format!(
+            "Cannot parse message of type string: {}",
+            e.to_string()
+        ))),
+    }
+}
